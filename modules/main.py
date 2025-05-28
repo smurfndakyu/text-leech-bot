fdd7418a90925bd9424882b036c553feebc0f056
smurfndakyu <bhumisingh22006@gmail.com>
2025-05-28 07:04:04 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -98,7 +98,7 @@ async def account_login(bot: Client, m: Message):
     x = await input.download()
     await input.delete(True)
 
-    thread_id = m.message_thread_id
+    thread_id = getattr(m, "message_thread_id", None)
 
     path = f"./downloads/{m.chat.id}"
 
