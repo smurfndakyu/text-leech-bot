c244ded9c3050f8dd11afc4dbb0d1914b28560c5
smurfndakyu <bhumisingh22006@gmail.com>
2025-05-28 06:57:21 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -92,12 +92,13 @@ async def restart_handler(_, m):
 
 
 @bot.on_message(filters.command(["upload"]))
-async def account_login(bot: Client, m: Message):
-    thread_id = m.message_thread_id
+async def account_login(bot: Client, m: Message)
     editable = await m.reply_text('𝐓𝐨 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐀 𝐓𝐱𝐭 𝐅𝐢𝐥𝐞 𝐒𝐞𝐧𝐝 𝐇𝐞𝐫𝐞 ⏍')
     input: Message = await bot.listen(editable.chat.id)
     x = await input.download()
     await input.delete(True)
+
+    thread_id = m.message_thread_id
 
     path = f"./downloads/{m.chat.id}"
 
