5dcf72aec257b2fb7646d268310d8d22d9a2646f
smurfndakyu <bhumisingh22006@gmail.com>
2025-05-29 22:39:37 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -210,7 +210,7 @@ async def account_login(bot: Client, m: Message):
                 ytf = f"b[height<={raw_text2}]/bv[height<={raw_text2}]+ba/b/bv+ba"
 
             if "stream.pwjarvis.app" in url:
-                cmd = f'yt-dlp -f "{ytf}" --download-sections "*14:" "{url}" -o "{name}.mp4"'
+                cmd = f'yt-dlp -f "{ytf}" --download-sections "*14.0-" "{url}" -o "{name}.mp4"'
             elif "jw-prod" in url:
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
             else:
